8e50f2e8239347ab57e0188379e5295fe00a6777
Jeremy Rodoli <jeremyjunior3003@gmail.com>
2026-07-31 17:09:39 -0400

    Proyecto base CRUD de mangas

---
+++ script.js
@@ -0,0 +1,108 @@
+const formulario = document.getElementById("mangaForm");
+const tabla = document.getElementById("tablaMangas");
+
+let mangas = JSON.parse(localStorage.getItem("mangas")) || [];
+let indiceEditar = -1;
+
+mostrarMangas();
+
+formulario.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    const manga = {
+
+        titulo: document.getElementById("titulo").value,
+
+        autor: document.getElementById("autor").value,
+
+        genero: document.getElementById("genero").value,
+
+        estado: document.getElementById("estado").value
+
+    };
+
+    if (indiceEditar === -1) {
+
+        mangas.push(manga);
+
+    } else {
+
+        mangas[indiceEditar] = manga;
+
+        indiceEditar = -1;
+
+    }
+
+    guardarDatos();
+
+    formulario.reset();
+
+});
+
+function mostrarMangas() {
+
+    tabla.innerHTML = "";
+
+    mangas.forEach((manga, index) => {
+
+        tabla.innerHTML += `
+
+        <tr>
+
+            <td>${manga.titulo}</td>
+
+            <td>${manga.autor}</td>
+
+            <td>${manga.genero}</td>
+
+            <td>${manga.estado}</td>
+
+            <td>
+
+                <button class="editar" onclick="editarManga(${index})">Editar</button>
+
+                <button class="eliminar" onclick="eliminarManga(${index})">Eliminar</button>
+
+            </td>
+
+        </tr>
+
+        `;
+
+    });
+
+}
+
+function guardarDatos() {
+
+    localStorage.setItem("mangas", JSON.stringify(mangas));
+
+    mostrarMangas();
+
+}
+
+function eliminarManga(index) {
+
+    if (confirm("¿Eliminar este manga?")) {
+
+        mangas.splice(index, 1);
+
+        guardarDatos();
+
+    }
+
+}
+
+function editarManga(index) {
+
+    const manga = mangas[index];
+
+    document.getElementById("titulo").value = manga.titulo;
+    document.getElementById("autor").value = manga.autor;
+    document.getElementById("genero").value = manga.genero;
+    document.getElementById("estado").value = manga.estado;
+
+    indiceEditar = index;
+
+}
